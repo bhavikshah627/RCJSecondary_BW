50faf0f248d9b1f5060a5996c2abd030e303b02f
Bhavik Shah <bhavikshah627@gmail.com>
2015-01-31 17:23:46 -0500

    Added getPing Functions

---
+++ Sensor Tests/PingVal.ino
@@ -0,0 +1,27 @@
+int getPingDist(int pingPin)
+{
+	// establish variables for duration of the ping, 
+	// and the distance result in inches and centimeters:
+	long duration, inches, cm;
+
+	pinMode(pingPin, OUTPUT);
+	digitalWrite(pingPin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(pingPin, HIGH);
+	delayMicroseconds(5);
+	digitalWrite(pingPin, LOW);
+
+	pinMode(pingPin, INPUT);
+	duration = pulseIn(pingPin, HIGH);
+
+	cm = microsecondsToCentimeters(duration);
+}
+
+
+long microsecondsToCentimeters(long microseconds)
+{
+	// The speed of sound is 340 m/s or 29 microseconds per centimeter.
+	// The ping travels out and back, so to find the distance of the
+	// object we take half of the distance travelled.
+	return microseconds / 29 / 2;
+}
